eef165ee9c1d4a1bbdb91b42b5cafd85240654c1
ultimate010 <ultimate010@gmail.com>
2013-06-24 10:01:10 +0800

    修改lab2中的bug，解决lab4无法通过的问题，lab4完成

--- code/my_lab4/kern/mm/default_pmm.c
+++ code/my_lab4/kern/mm/default_pmm.c
@@ -100,8 +100,9 @@ default_alloc_pages(size_t n) {
         if (page->property > n) {
             struct Page *p = page + n;
             p->property = page->property - n;
+            SetPageProperty(p);
             list_add(&free_list, &(p->page_link));
-    }
+        }
         nr_free -= n;
         ClearPageProperty(page);
     }
